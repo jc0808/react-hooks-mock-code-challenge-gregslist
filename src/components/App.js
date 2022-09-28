005e802da93669fa3edfc320971974703ea94121
Joshua Campos <jdeveloper1203@gmail.com>
2022-09-28 11:46:07 -0400

    s

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([]);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:6001/listings')
+        .then(response => response.json())
+        .then(data => setListings(data));
+    }, 1000)
+
+
+
+  }, [])
+
+  function handleDelete(id) {
+    const filtered = listings.filter(listing => { return listing.id !== id });
+
+    setListings(filtered)
+  }
+
+
+
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} handleDelete={handleDelete} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,24 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
+import { v1 } from "uuid";
 
-function ListingsContainer() {
+function ListingsContainer({ listings, handleDelete }) {
+
+  const displayListings = listings.map(listing => {
+    return (<ListingCard key={v1()} description={listings.description}
+      image={listing.image} location={listing.location} handleDelete={handleDelete} id={listing.id} />)
+  })
+
   return (
     <main>
       <ul className="cards">
         {/* use the ListingCard component to display listings */}
+        {displayListings}
+
       </ul>
     </main>
   );
+
 }
 
 export default ListingsContainer;
